fa097225338e92e525fd33c49db09ff41633a5ea
Arthurprogrammer404 <95263335+Arthurprogrammer404@users.noreply.github.com>
2021-12-21 14:09:06 -0300

    Add files via upload

---
+++ index.html/index.html/mobile-navbar.js
@@ -0,0 +1,44 @@
+class MobileNavBar{
+    constructor(mobileMenu, navlist, navlinks){
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorALL(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks(){
+        this.navLinks.forEach((link, index) => {
+          link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.5s ease forwards
+            {index / 7 + 0.3}s`);
+        });
+    }
+
+    handleClick(){
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent(){
+        this.mobileMenu.addEventListener("click", this.handleClick);
+        this.animateLinks();
+    }
+
+    init(){
+        if(this.mobileMenu){
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const MobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+);
+MobileNavbar.init();
